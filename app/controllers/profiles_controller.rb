bb8d1814e61b064f216c3436311acdf9f51faca7
Elisangela <elisangela00silva@gmail.com>
2020-11-20 20:39:55 -0300

    exibição na tela dos perfis criados

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,25 @@
 class ProfilesController < ApplicationController
-  def index 
+  def index
+    @profiles = Profile.all 
   end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params).save
+    redirect_to profiles_path
+  end
+
+  def show
+
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name).merge({user_id:current_user.id})
+  end
+
 end

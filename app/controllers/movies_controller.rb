ce9e12c1a3dd31d1f6669e79fcfd5d86fbd4f6af
Elisangela <elisangela00silva@gmail.com>
2020-11-22 11:57:59 -0300

    finalização da aplicação busca e visualização dos resultados

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,6 @@ class MoviesController < ApplicationController
   def search
     busca = RestClient.get 'https://api.themoviedb.org/3/search/movie', { params: { api_key: '9dfb9439b9b75b6bfb4562a044d050ba', query: params[:filme] } }
 
-    @movies = busca.body
+    @movies = busca.body.to_json
   end
 end

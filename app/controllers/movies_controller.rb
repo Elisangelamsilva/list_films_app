8e5f139f1d83a7f85c07a2dc2b35398a7ab833b7
Elisangela <elisangela00silva@gmail.com>
2020-11-27 12:48:04 -0300

    action watch: permitindo os paramentros passado e conversão de alguns

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,32 @@ class MoviesController < ApplicationController
   def search
     busca = RestClient.get 'https://api.themoviedb.org/3/search/movie', { params: { api_key: '9dfb9439b9b75b6bfb4562a044d050ba', query: params[:filme] } }
 
-    @movies = busca.body.to_json
+    
+    @movies = JSON.parse(busca.body)
+      
   end
+
+  # listar db do model => model.all
+  # 
+  def watch
+    watch = Movie.new(movie_params)
+
+  end
+
+  def watched
+  end
+
+  private
+
+  def movie_params
+    permitted = params.permit(:title,:original_language,:overview,:popularity,:poster_path,:adult,:id,:release_date)
+    permitted.tap do |p|
+      p[:popularity] = p[:popularity]
+      p[:api_id] = p["id"]
+      p.delete(:id)
+      p[:release_at] = p["release_date"]
+      p.delete(:release_date)
+    end
+  end
+
 end

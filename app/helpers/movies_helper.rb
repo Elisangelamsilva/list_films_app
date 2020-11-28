58603ae3949a00d4480ab2ee4c86ba13de886d23
Elisangela <elisangela00silva@gmail.com>
2020-11-28 12:27:12 -0300

    ajustes no modal e codigo encapsulado no helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,7 +1,7 @@
 module MoviesHelper
-  def exibir_poster(movie)
-    if movie.poster_path
-      "https://image.tmdb.org/t/p/original/#{movie.poster_path}"
+  def exibir_poster(poster_path)
+    if poster_path
+      "https://image.tmdb.org/t/p/original/#{poster_path}"
     else
       'https://www.joblo.com/assets/images/joblo/database-specific-img-225x333.jpg'
     end
